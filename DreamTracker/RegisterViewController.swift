3c9e3720f919e2cb8d35e2ddb689cee4fa672914
Renny Runiawati <renny.runiawati@tokopedia.com>
2018-11-16 09:39:43 +0700

    Register and login training preparation

---
+++ DreamTracker/RegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterViewController.swift
+//  DreamTracker
+//
+//  Created by Nakama on 16/11/18.
+//  Copyright © 2018 Tokopedia. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tapRegisterButton(_ sender: UIBarButtonItem) {
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
